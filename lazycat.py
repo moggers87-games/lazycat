5e25640f0f9b4c60c53688d0a53affdb5874840c
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-11-01 15:16:58 +0000

    Follow the mouse!

--- lazycat.py
+++ lazycat.py
@@ -7,27 +7,42 @@ CAT_WIDTH = 200
 CAT_HEIGHT = 212
 CAT_POSITION = ((SCREEN_WIDTH - CAT_WIDTH) / 2, (SCREEN_HEIGHT - CAT_HEIGHT) / 2)
 
-clock = pg.time.Clock()
-
 def music():
     pg.mixer.init()
     pg.mixer.music.load("gaslampfunworks.ogg")
     pg.mixer.music.play(-1)
 
 def loop():
+    global CAT_POSITION
+
     while True:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+        if pg.mouse.get_focused():
+            CAT_POSITION = set_cat_from_mouse()
+        draw()
         clock.tick(15)
 
 def draw():
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pg.DOUBLEBUF)
-    cat = pg.image.load('lasercat.png')
-    cat = pg.transform.smoothscale(cat, (CAT_WIDTH, CAT_HEIGHT))
     screen.blit(cat, CAT_POSITION)
     pg.display.flip()
+
+def set_cat_from_mouse():
+    pos = pg.mouse.get_pos()
+    pos = (pos[0] - CAT_WIDTH/2, pos[1] -  CAT_HEIGHT/2)
+    return pos
+
+clock = pg.time.Clock()
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pg.DOUBLEBUF)
+cat = pg.image.load('lasercat.png')
+cat = pg.transform.smoothscale(cat, (CAT_WIDTH, CAT_HEIGHT))
+
+pg.mouse.set_visible(False)
+pg.mouse.set_pos(set_cat_from_mouse())
+
 
 if __name__ == '__main__':
     draw()
