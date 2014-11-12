99a2264204c831b3daf8bdf5e5bf0b01a997a06f
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-11-12 01:14:32 +0000

    Add multiple mouses
    Also, objectify mouses

--- lazycat.py
+++ lazycat.py
@@ -11,9 +11,48 @@ SCREEN_HEIGHT = 600
 CAT_WIDTH = 200
 CAT_HEIGHT = 212
 CAT_POSITION = ((SCREEN_WIDTH - CAT_WIDTH) // 2, (SCREEN_HEIGHT - CAT_HEIGHT) // 2)
-MOUSE_POSITION = CAT_POSITION
-MOUSE_DIRECTION = 0 # 0 left; 1 right; 2 down; 3 up
 LASER_RANGE = math.floor(CAT_WIDTH * 1.5)
+
+class Mouse(object):
+    """A mouse!"""
+    position = CAT_POSITION
+
+    def __init__(self):
+        self.image = pg.image.load('lasermouse.png')
+        self.image = pg.transform.smoothscale(self.image,
+            (CAT_WIDTH // 3, CAT_HEIGHT // 3)
+        )
+
+    @property
+    def direction(self):
+        # 0 left; 1 right; 2 down; 3 up
+        return getattr(self, "_direction", 0)
+
+    @direction.setter
+    def direction(self, value):
+        self._direction = value % 4
+
+    @property
+    def center(self):
+        pos = self.position
+        pos = (pos[0] + CAT_WIDTH//6, pos[1] +  CAT_HEIGHT//6) # mouse is 1/3 cat
+        return pos
+
+    def run_away(self):
+        padding = 60
+        d = random.randint(1, 10)
+        pos = self.position
+        if self.direction == 0:
+            pos = (((pos[0] - d + padding) % SCREEN_WIDTH) - padding, pos[1])
+        elif self.direction == 1:
+            pos = (((pos[0] + d + padding) % SCREEN_WIDTH) - padding, pos[1])
+        elif self.direction == 2:
+            pos = (pos[0], ((pos[1] - d + padding) % SCREEN_HEIGHT) - padding)
+        elif self.direction == 3:
+            pos = (pos[0], ((pos[1] + d + padding) % SCREEN_HEIGHT) - padding)
+        self.position = pos
+
+MICE = [Mouse() for i in range(4)]
 
 def music():
     pg.mixer.init()
@@ -22,8 +61,6 @@ def music():
 
 def loop():
     global CAT_POSITION
-    global MOUSE_POSITION
-    global MOUSE_DIRECTION
 
     while True:
         for e in pg.event.get():
@@ -39,9 +76,11 @@ def loop():
         if pg.mouse.get_focused():
             CAT_POSITION = set_cat_after_mouse()
 
-        if random.randint(0, 30) == 0:
-            MOUSE_DIRECTION = random.randint(0, 3)
-        MOUSE_POSITION = run_mouse_run()
+        for mouse in MICE:
+            if random.randint(0, 30) == 0:
+                mouse.direction = random.randint(0, 3)
+            mouse.run_away()
+
         draw()
         clock.tick(24)
 
@@ -49,52 +88,39 @@ def draw():
     screen.fill((0, 0, 0))
     screen.blit(cat, CAT_POSITION)
 
-    if MOUSE_DIRECTION == 0:
-        r_mouse = mouse
-    if MOUSE_DIRECTION == 1:
-        r_mouse = pg.transform.rotate(mouse, 90)
-    if MOUSE_DIRECTION == 2:
-        r_mouse = pg.transform.rotate(mouse, 180)
-    if MOUSE_DIRECTION == 3:
-        r_mouse = pg.transform.rotate(mouse, 270)
+    firing = False
+    for mouse in MICE:
+        rotation = 90 * mouse.direction
+        r_mouse = pg.transform.rotate(mouse.image, rotation)
+        r_mouse = pg.transform.rotate(r_mouse, random.randint(-5, 5))
 
-    r_mouse = pg.transform.rotate(r_mouse, random.randint(-5, 5))
+        screen.blit(r_mouse, mouse.position)
 
-    screen.blit(r_mouse, MOUSE_POSITION)
-    cat_pos, mouse_pos, in_range = cat_laser_in_range(cat_center(), mouse_center())
-    if in_range and True in pg.mouse.get_pressed():
-        # magic numbers, manually guesstimated for eye centers
-        pg.draw.line(screen, (255, 0, 0), (cat_pos[0]+42, cat_pos[1]), mouse_pos, 5)
-        pg.draw.line(screen, (255, 0, 0), (cat_pos[0]-22, cat_pos[1]), mouse_pos, 5)
+        if True not in pg.mouse.get_pressed():
+            if firing:
+                firing = False
+            continue
+        elif firing:
+            continue
+
+        cat_pos, mouse_pos, in_range = cat_laser_in_range(cat_center(), mouse.center)
+        if in_range:
+            # magic numbers, manually guesstimated for eye centers
+            pg.draw.line(screen, (255, 0, 0), (cat_pos[0]+42, cat_pos[1]), mouse_pos, 5)
+            pg.draw.line(screen, (255, 0, 0), (cat_pos[0]-22, cat_pos[1]), mouse_pos, 5)
+            firing = True
+
     pg.display.flip()
 
 def set_cat_after_mouse():
+    """Set cat to follow where the pointer would be"""
     pos = pg.mouse.get_pos()
     pos = (pos[0] - CAT_WIDTH//2, pos[1] - CAT_HEIGHT//2)
-    return pos
-
-def run_mouse_run():
-    padding = 60
-    d = random.randint(1, 10)
-    pos = MOUSE_POSITION
-    if MOUSE_DIRECTION == 0:
-        pos = (((pos[0] - d + padding) % SCREEN_WIDTH) - padding, pos[1])
-    elif MOUSE_DIRECTION == 1:
-        pos = (((pos[0] + d + padding) % SCREEN_WIDTH) - padding, pos[1])
-    elif MOUSE_DIRECTION == 2:
-        pos = (pos[0], ((pos[1] - d + padding) % SCREEN_HEIGHT) - padding)
-    elif MOUSE_DIRECTION == 3:
-        pos = (pos[0], ((pos[1] + d + padding) % SCREEN_HEIGHT) - padding)
     return pos
 
 def cat_center():
     pos = CAT_POSITION
     pos = (pos[0] + CAT_WIDTH//2, pos[1] + CAT_HEIGHT//2)
-    return pos
-
-def mouse_center():
-    pos = MOUSE_POSITION
-    pos = (pos[0] + CAT_WIDTH//6, pos[1] +  CAT_HEIGHT//6) # mouse is 1/3 cat
     return pos
 
 def cat_laser_in_range(cat_pos, mouse_pos):
@@ -109,8 +135,6 @@ pg.display.set_caption("L͏̷a̶͜z̸͘e҉̢ŕ͡G̴̶ư͡n͏͟!̨̕ H̶̶é͡
 
 cat = pg.image.load('lasercat.png')
 cat = pg.transform.smoothscale(cat, (CAT_WIDTH, CAT_HEIGHT))
-mouse = pg.image.load('lasermouse.png')
-mouse = pg.transform.smoothscale(mouse, (CAT_WIDTH // 3, CAT_HEIGHT // 3))
 
 pg.mouse.set_visible(False)
 pg.mouse.set_pos(set_cat_after_mouse())
